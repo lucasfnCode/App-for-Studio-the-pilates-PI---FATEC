1e52367759ae9eb3bcffcad5399885bcada8b3e1
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-05-14 22:29:52 -0300

    CHORE: criação do projeto java para o backend

---
+++ backend/src/main/java/br/com/semesperanca/app/managing/pilates/studios/AppManagingPilatesStudiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.semesperanca.app.managing.pilates.studios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppManagingPilatesStudiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppManagingPilatesStudiosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/semesperanca/app/managing/pilates/studios/AppManagingPilatesStudiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.semesperanca.app.managing.pilates.studios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppManagingPilatesStudiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
