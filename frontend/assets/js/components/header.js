f3c3b514a7294fc9d75647265bca267772a3db1a
Gabriel Gadelha <gabrielgadelha8@hotmail.com>
2025-05-23 10:36:28 -0300

    :lipstick: Header
    Alteração no header pra corresponder ao mockup

--- frontend/assets/js/components/header.js
+++ frontend/assets/js/components/header.js
@@ -1,10 +1,17 @@
 export const headerHtml = `
-<nav class="d-flex justify-content-between align-items-center px-4 py-2 border-bottom">
+
+<section class="d-flex justify-content-center" id="head">
+
+<h1>Estudio de Pilates</h1>
+
+</section>
+
+<nav class="d-flex justify-content-between align-items-center px-4 py-2 border-bottom"  id="nav">
 
   <div class="nav nav-underline">
     <a class="nav-link m-1 text-secondary" aria-current="page" href="#home">Home</a>
     <a class="nav-link m-1 text-secondary" href="#">Agendamento</a>
-    <a class="nav-link m-1 text-secondary" href="#">Link</a>
+    <a class="nav-link m-1 text-secondary" href="#">Aulas</a>
   </div>
 
   <div class="nav">
--- frontend/dist/frontend.4e1ccf09.js
+++ frontend/dist/frontend.4e1ccf09.js
@@ -3831,12 +3831,19 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "headerHtml", ()=>headerHtml);
 const headerHtml = `
-<nav class="d-flex justify-content-between align-items-center px-4 py-2 border-bottom">
+
+<section class="d-flex justify-content-center" id="head">
+
+<h1>Estudio de Pilates</h1>
+
+</section>
+
+<nav class="d-flex justify-content-between align-items-center px-4 py-2 border-bottom"  id="nav">
 
   <div class="nav nav-underline">
     <a class="nav-link m-1 text-secondary" aria-current="page" href="#home">Home</a>
     <a class="nav-link m-1 text-secondary" href="#">Agendamento</a>
-    <a class="nav-link m-1 text-secondary" href="#">Link</a>
+    <a class="nav-link m-1 text-secondary" href="#">Aulas</a>
   </div>
 
   <div class="nav">
